a8712bb33d8596b6a57ea3c87ca884806f5ba363
AInine <ainine.jp@gmail.com>
2021-09-05 19:39:53 +0900

    initial commit

---
+++ src/main/java/WhoJoinedBot.java
@@ -0,0 +1,26 @@
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.entity.message.MessageDecoration;
+
+public class WhoJoinedBot {
+    public static void main(String[] args) {
+        DiscordApi api = new DiscordApiBuilder()
+                .setToken(System.getenv().get("DISCORD_TOKEN"))
+                .login().join();
+
+        api.addServerVoiceChannelMemberJoinListener(event -> {
+            String user = event.getUser().getName();
+            String channel = event.getChannel().getName();
+
+            if (event.isMove()) return;
+
+            new MessageBuilder()
+                    .append(user, MessageDecoration.UNDERLINE)
+                    .append(" が ")
+                    .append(channel, MessageDecoration.BOLD)
+                    .append(" に入ったよん")
+                    .send(api.getTextChannelsByName(System.getenv().get("CHANNEL_NAME")).iterator().next());
+        });
+    }
+}
