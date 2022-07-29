ae8b1a5688c543c5baa622a76751a836b61f924f
AInine <ainine.jp@gmail.com>
2022-07-30 01:36:22 +0900

    change: bot tells user's nickname

--- src/main/java/WhoJoinedBot.java
+++ src/main/java/WhoJoinedBot.java
@@ -1,7 +1,17 @@
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.channel.Channel;
+import org.javacord.api.entity.channel.ServerChannel;
 import org.javacord.api.entity.message.MessageBuilder;
 import org.javacord.api.entity.message.MessageDecoration;
+import org.javacord.api.entity.user.User;
+import org.javacord.api.interaction.SlashCommand;
+import org.javacord.api.interaction.SlashCommandInteraction;
+import org.javacord.api.interaction.SlashCommandOption;
+import org.javacord.api.interaction.SlashCommandOptionType;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class WhoJoinedBot {
     public static void main(String[] args) {
@@ -9,8 +19,24 @@ public class WhoJoinedBot {
                 .setToken(System.getenv().get("DISCORD_TOKEN"))
                 .login().join();
 
+        SlashCommand command = SlashCommand.with("setChannel", "Set the channel bot sends messages",
+                        List.of(
+                                SlashCommandOption.create(SlashCommandOptionType.CHANNEL, "CHANNEL", "channel", true)
+                        ))
+                .createGlobal(api)
+                .join();
+
+        api.addSlashCommandCreateListener(event -> {
+            SlashCommandInteraction slashCommandInteraction = event.getSlashCommandInteraction();
+            ServerChannel channel = slashCommandInteraction.getFirstOptionChannelValue().orElse(null);
+
+            slashCommandInteraction.createImmediateResponder()
+                    .setContent("通知チャンネルが変更されました")
+                    .respond();
+        });
+
         api.addServerVoiceChannelMemberJoinListener(event -> {
-            String user = event.getUser().getName();
+            String user = event.getUser().getNickname(event.getServer()).get();
             String channel = event.getChannel().getName();
 
             if (event.isMove()) return;
